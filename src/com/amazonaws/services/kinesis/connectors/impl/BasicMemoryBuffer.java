b0ccb22281afa0b2c15dd7e1f785e00fa0776bd1
James Siri <jamesiri@amazon.com>
2019-04-09 10:46:49 -0700

    Update BasicMemoryBuffer.java

--- src/com/amazonaws/services/kinesis/connectors/impl/BasicMemoryBuffer.java
+++ src/com/amazonaws/services/kinesis/connectors/impl/BasicMemoryBuffer.java
@@ -1,5 +1,5 @@
 /*
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0.
  */
 package com.amazonaws.services.kinesis.connectors.impl;
